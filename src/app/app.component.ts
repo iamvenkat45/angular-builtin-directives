609b2c1d31b4856dd13f97d0a3de88aa693d5dfc
Venkat <venkatasandeep.bandlamudi@aig.com>
2018-07-30 07:06:42 -0400

    Initial Commit..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-directives';
+  title = 'Angular Built in Directives';
+  users = [{
+    firstName: 'Venkat',
+    lastname: 'Sandeep',
+    age: 20,
+    subjects: ['Maths', 'Physics', 'English']
+  }, {
+    firstName: 'Vijay',
+    lastname: 'Soppa',
+    age: 18,
+    subjects: ['Maths', 'Physics', 'English']
+  }, {
+    firstName: 'Sri',
+    lastname: 'B',
+    age: 19,
+    subjects: ['Maths', 'Physics', 'English']
+  }, {
+    firstName: 'jai',
+    lastname: 'M',
+    age: 17,
+    subjects: ['Maths', 'Physics', 'English']
+  }];
+
+  canShowSubjects(user) {
+    return user.age <= 18;
+  }
 }
